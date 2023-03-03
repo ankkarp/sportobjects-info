9d4f0f6eb6fda3695f8c7c83ac6100f5e38cd9bc
ankkarp <anna.a.k.2002@gmail.com>
2023-03-03 09:14:00 +0300

    funding and sporttype endpoints

--- server/run.py
+++ server/run.py
@@ -1,6 +1,5 @@
 import os
 
-import uvicorn
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -20,11 +19,44 @@ engine = create_engine("postgresql://{user}:{psw}@{host}:{port}/{db}".format(
 con = engine.connect()
 
 
+@app.get("/data")
+async def get_all():
+    df = pd.read_sql(text(f'SELECT * FROM sportobjects'), con)
+    return df.astype(str).to_dict('records')
+
+
+@app.get("/ids")
+async def read_object():
+    df = pd.read_sql(text(f'SELECT id FROM sportobjects'), con)
+    return df.to_dict()
+
+
 @app.get("/id/{id}")
 async def read_object(id: int):
     object_df = pd.read_sql(
         text(f'SELECT * FROM sportobjects WHERE id = {id}'), con).iloc[0]
     return object_df.to_dict()
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host='0.0.0.0', reload=True, proxy_headers=True)
+
+@app.get("/funding")
+async def read_object():
+    df = pd.read_sql(text(f'''SELECT funding_from_the_federal_budget,
+                          funding_from_the_federal_budget_of_which_mastered
+                          funding_from_the_budget_of_the_subject_of_the_federation,
+                          funding_from_the_budget_of_the_subject_of_the_federation_of_whi,
+                          funding_from_the_budget_of_the_municipality,
+                          funding_from_the_budget_of_the_municipality_of_which_mastered,
+                          funding_from_extrabudgetary_sources,
+                          funding_from_extrabudgetary_sources_of_which_mastered
+                          FROM sportobjects'''), con)
+    return {col: df[col].sum(skipna=True) for col in df.columns}
+
+
+@app.get("/sporttype")
+async def read_object():
+    df = pd.read_sql(
+        text(f'''SELECT type_of_sports_complex FROM sportobjects'''), con)
+    return df['type_of_sports_complex'].value_counts().to_dict()
+
+
+# @app.get()s
